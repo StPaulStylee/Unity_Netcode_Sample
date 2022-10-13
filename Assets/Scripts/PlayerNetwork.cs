290c6bc943542a6e236232e2d0ffc43ffbf48f3d
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2022-10-13 06:36:37 -0500

    Using NetworkVariable in a client authoritative manner to update data across the host/client

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -4,9 +4,21 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour {
+  private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+  public override void OnNetworkSpawn() {
+    randomNumber.OnValueChanged += (int previousValue, int newValue) => {
+      // OwnerClientId is part of NetworkBehavior
+      Debug.Log($"{OwnerClientId}; {randomNumber.Value}");
+    };
+  }
+
   private void Update() {
     if (!IsOwner) {
       return;
+    }
+    if (Input.GetKeyDown(KeyCode.T)) {
+      randomNumber.Value = Random.Range(0, 100);
     }
     Vector3 moveDirection = Vector3.zero;
     if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
